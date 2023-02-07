5a55adf5f478158dcb3609290f00a3dd0996fb69
Abhinav Rastogi <rastogi.abhinav8@gmail.com>
2023-02-07 15:37:33 +0530

    initial-commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const  express = require('express');
+const cors = require("cors")
+
+const app = express();
+
+const port =process.env.PORT || 3000;
+
+app.use(cors());
+
+
+app.get("/", (req, res)=>{
+    res.send('m alive')
+})
+
+app.listen(port , ()=>{
+    console.log('hello , world')
+})
